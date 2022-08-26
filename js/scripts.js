5acf8fdeb3dd6e827f5040d584f07e6c867de3cb
LekenSuvar <87902339+LekenSuvar@users.noreply.github.com>
2022-08-26 11:16:14 +0400

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -40,19 +40,38 @@ function burgerMenu() {
   function fixedNav() {
     const nav = document.querySelector('nav'),
         hero = document.querySelector('.hero');
-  
     // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
-    const breakpoint = Math.floor(window.screen.height);
-    console.log(nav.clientHeight)
+    const breakpoint = 80;
     if (window.scrollY >= breakpoint) {
+      hero.style.paddingTop = 4 + nav.clientHeight / 10 + 'rem'
       nav.classList.add('fixed__nav')
-      hero.style.paddingTop = nav.clientHeight + 'px'
     } else {
+      hero.style.paddingTop = 4 + 'rem'
       nav.classList.remove('fixed__nav')
-      hero.style.paddingTop = 0 + 'px'
     }
   }
   window.addEventListener('scroll', fixedNav)
 
   ;
+document.addEventListener('DOMContentLoaded', ()=> {
+    const p = document.querySelectorAll('p');
+    p.forEach(item => {
+        item.classList.add('hide');
+    });
+    
+    const sense = 100;
+    function animation(){
+        p.forEach(item => {
+            if(window.pageYOffset + window.screen.height - sense >= item.offsetTop + item.offsetHeight){
+                item.classList.remove('hide');
+                item.classList.add('show');
+            }
+        })
+    }
+    animation()
+    window.addEventListener('scroll', () => {
+        animation()
+    });
+})
+;
 
